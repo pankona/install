3ed76953865bf988081681d7975e1dd9fdf1c76a
pankona <yosuke.akatsuka@gmail.com>
2022-01-08 17:57:28 +0000

    use 0.0.5

--- install.sh
+++ install.sh
@@ -3,9 +3,8 @@
 mkdir /tmp/install
 cd /tmp/install
 
-# This example uses 0.0.4 but let's choose latest version
-wget https://github.com/pankona/install/releases/download/v0.0.4/install_0.0.4_Linux_x86_64.tar.gz
-tar zxf install_0.0.4_Linux_x86_64.tar.gz
+wget https://github.com/pankona/install/releases/download/v0.0.5/install_0.0.5_Linux_x86_64.tar.gz
+tar zxf install_0.0.5_Linux_x86_64.tar.gz
 
 ./install asdf
 ./install bashrc
