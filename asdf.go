c45c0f6884ec56d678c8598426251d05c43a7202
pankona <yosuke.akatsuka@gmail.com>
2022-01-08 16:34:11 +0000

    remove source

--- asdf.go
+++ asdf.go
@@ -40,10 +40,10 @@ func installToolsViaAsdf(ctx context.Context) {
 	}
 	ec := errContainer{}
 	for _, tool := range tools {
-		ec.execCommand(ctx, currentDir, "bash", "-c", "source "+filepath.Join(homeDir(), ".bashrc")+"; asdf plugin-add "+tool.name)
+		ec.execCommand(ctx, currentDir, "asdf", "plugin-add", tool.name)
 		ec.err = nil // ignore error to ignore "the plugin is already added"
-		ec.execCommand(ctx, currentDir, "bash", "-c", "source "+filepath.Join(homeDir(), ".bashrc")+"; asdf install "+tool.name+" "+tool.version)
-		ec.execCommand(ctx, currentDir, "bash", "-c", "source "+filepath.Join(homeDir(), ".bashrc")+"; asdf global "+tool.name+" "+tool.version)
+		ec.execCommand(ctx, currentDir, "asdf", "install", tool.name, tool.version)
+		ec.execCommand(ctx, currentDir, "asdf", "global", tool.name, tool.version)
 	}
 	if ec.err != nil {
 		log.Fatal(ec.err)
--- prettier.go
+++ prettier.go
@@ -3,13 +3,12 @@ package main
 import (
 	"context"
 	"log"
-	"path/filepath"
 )
 
 func installPrettier(ctx context.Context) {
 	ec := errContainer{}
 	ec.execCommand(ctx, currentDir, "yarn", "global", "add", "prettier")
-	ec.execCommand(ctx, currentDir, "bash", "-c", "source "+filepath.Join(homeDir(), ".bashrc")+"; asdf reshim")
+	ec.execCommand(ctx, currentDir, "asdf", "reshim")
 	if ec.err != nil {
 		log.Fatal(ec.err)
 	}
