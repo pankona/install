438c4cd4f6f316d9ad1d576732304d83a88d251a
pankona <yosuke.akatsuka@gmail.com>
2022-01-18 16:05:06 +0900

    add missing dependencies for apt-add-repository

--- gh.go
+++ gh.go
@@ -9,6 +9,7 @@ func installGitHubCLI(ctx context.Context) {
 	ec := errContainer{}
 
 	ec.execCommand(ctx, currentDir, "sudo", "apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-key", "C99B11DEB97541F0")
+	ec.execCommand(ctx, currentDir, "sudo", "install", "software-properties-common")
 	ec.execCommand(ctx, currentDir, "sudo", "apt-add-repository", "https://cli.github.com/packages", "-y")
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "update")
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "gh")
