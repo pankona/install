bb7e2b0ade1808ec10d7003e257a3980cbea8c40
pankona <yosuke.akatsuka@gmail.com>
2022-01-18 15:51:22 +0900

    add missing hyphon

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd /tmp/install
 wget https://github.com/pankona/install/releases/download/v0.0.7/install_0.0.7_Linux_x86_64.tar.gz
 tar zxf install_0.0.7_Linux_x86_64.tar.gz
 
-./install asdf
-./install bashrc
+./install -asdf
+./install -bashrc
 source ~/.bashrc
-./install all
+./install -all
