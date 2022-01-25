814b7291d4197bdf42bee74a925b1be394119303
pankona <yosuke.akatsuka@gmail.com>
2022-01-25 05:31:49 +0000

    add missing -y for installing github cli

--- gh.go
+++ gh.go
@@ -9,10 +9,10 @@ func installGitHubCLI(ctx context.Context) {
 	ec := errContainer{}
 
 	ec.execCommand(ctx, currentDir, "sudo", "apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-key", "C99B11DEB97541F0")
-	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "software-properties-common")
+	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "software-properties-common", "-y")
 	ec.execCommand(ctx, currentDir, "sudo", "apt-add-repository", "https://cli.github.com/packages", "-y")
-	ec.execCommand(ctx, currentDir, "sudo", "apt", "update")
-	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "gh")
+	ec.execCommand(ctx, currentDir, "sudo", "apt", "update", "-y")
+	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "gh", "-y")
 	if ec.err != nil {
 		log.Fatal(ec.err)
 	}
