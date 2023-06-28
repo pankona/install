13b43ff94641f04d3f8d4ba933202819c7748d04
Yosuke Akatsuka <yosuke.akatsuka@gmail.com>
2023-06-28 18:03:27 +0900

    Update apttools.go

--- apttools.go
+++ apttools.go
@@ -12,9 +12,12 @@ func installAptTools(ctx context.Context) {
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "-y", "silversearcher-ag")
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "-y", "less")
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "-y", "locales-all")
+
+	// 後々 apt install で必要になるものはここで適当に入れちゃう
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "-y", "unzip")
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "-y", "libssl-dev")
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "-y", "libz-dev")
+	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "-y", "libexpat-dev")
 	// https: //github.com/cli/cli/discussions/6222
 	ec.execCommand(ctx, currentDir, "sudo", "apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys", "23F3D4EA75716059")
 	if ec.err != nil {
