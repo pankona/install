1d7ac3dc3306e2c5c8841472ff825a7d490a89f4
pankona <yosuke.akatsuka@gmail.com>
2024-09-28 12:05:00 +0000

    remove unnecessary commands

--- gh.go
+++ gh.go
@@ -8,9 +8,6 @@ import (
 func installGitHubCLI(ctx context.Context) {
 	ec := errContainer{}
 
-	ec.execCommand(ctx, currentDir, "sudo", "apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-key", "C99B11DEB97541F0")
-	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "software-properties-common", "-y")
-	ec.execCommand(ctx, currentDir, "sudo", "apt-add-repository", "https://cli.github.com/packages", "-y")
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "update", "-y")
 	ec.execCommand(ctx, currentDir, "sudo", "apt", "install", "gh", "-y")
 	if ec.err != nil {
